5c1e283d32f130c435b0fc362c29bcab87dd7669
JaeGeun.Song <jae99c@gmail.com>
2021-10-18 16:33:14 +0900

    Implement Sign-Up feature

--- security1/src/main/java/com/song/security1/controller/IndexController.java
+++ security1/src/main/java/com/song/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.song.security1.controller;
 
+import ch.qos.logback.core.joran.spi.ConsoleTarget;
+import com.song.security1.model.User;
+import com.song.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //View를 리턴하겠다.
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     //localhost:8080
     //localhost:8080
@@ -29,16 +41,25 @@ public class IndexController {
     }
 
     // 스프링 시큐리티가 해당 주소를 낚아 챈다. -SecurityConfig 파일 생성 후 작동 안함.
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+
+    @GetMapping("/joinForm") // 회원가입 폼으로 이동
+    public String joinForm(){
+        return "joinForm";
     }
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨!";
+
+    @PostMapping("/join") //실제로 회원가입 수행
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER"); //ROLE_USER 라고 해서 강제로 넣어준다.
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword); //패스워드 암호화
+        user.setPassword(encPassword); // 암호화된 패스워드 넣어줌.
+
+        userRepository.save(user); //회원가입 잘됨. 비밀번호 1234 => 시큐리티로 로그인 할 수 없음 => 해쉬화되지 않았기 때문에..
+        return "redirect:/loginForm";
     }
 }
---
+++ security1/src/main/java/com/song/security1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.song.security1.repository;
+
+import com.song.security1.model.User;
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//CRUD 함수를 JpaRepository가 들고 있음.
+// @Repository라는 어노테이션이 없어도 IoC 되요. 이유는 JpaRepository를 상송했기 때문에...
+public interface UserRepository extends JpaRepository<User, Integer>{ // 자동으로 bin으로 등록이 된다.
+}
