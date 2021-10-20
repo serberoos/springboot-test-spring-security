ae6fba1f3787c64a47cb1c02ccfcff68a649acf8
JaeGeun.Song <jae99c@gmail.com>
2021-10-20 11:58:58 +0900

    Google OAuthApi Implementation and Preferences

--- security1/src/main/java/com/song/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/song/security1/config/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줍니다.
-                .defaultSuccessUrl("/"); // loginForm으로 들어오면 / 페이지로 보내주고 그외 페이지로 요청하게 되면 그쪽으로 보내준다.
+                .defaultSuccessUrl("/") // loginForm으로 들어오면 / 페이지로 보내주고 그외 페이지로 요청하게 되면 그쪽으로 보내준다.
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요함.
     }
 
 }
